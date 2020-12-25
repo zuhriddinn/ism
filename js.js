48e774038ab4401715d3a62fe81fd986b6f89cad
Zuhriddin <nuriddinovsirojoddin@gmail.com>
2020-12-25 16:38:31 +0500

    versiya1js6

---
+++ js.js
@@ -0,0 +1,18 @@
+let a = 10
+
+for (i = 1; i <= a; i++) {
+
+    let ism = prompt(' Foydalanuvchining ismi ')
+    let yosh = +prompt(' Foydalanuvchining yoshi ')
+
+    let foydalanuvchila = {
+        Ismi: ism,
+        Yoshi: yosh,
+        Soni: a
+
+    }
+    console.log( i + (' inchi foydalanuvchi'));
+    console.log( ' Foydalanuvchining Ismi ' + foydalanuvchila.Ismi , ' Foydalanuvchining Yoshi ' +  foydalanuvchila.Yoshi);
+}
+
+
